81d117294f581610b6e5ceb7941e98a9b34a77a0
younus <younusmamunu@gmail.com>
2022-11-15 16:27:54 +0600

    second comit

---
+++ index1.php
@@ -0,0 +1,17 @@
+<?php
+include 'index.php';
+
+
+//echo $result;
+
+//$jsonData = json_decode($result);
+
+// var_dump( $jsonData);
+// print_r($jsonData);
+
+//echo '<pre>' , var_dump($jsonData) , '</pre>';
+
+
+// echo "<pre>";
+// print_r($jsonData);
+// echo "</pre>";
